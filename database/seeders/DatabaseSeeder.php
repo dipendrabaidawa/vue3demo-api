47fca34c89b54ae9a3a4db7a711015dfba2fa321
struk18 <karlikboroda@gmail.com>
2021-09-17 18:36:42 +0300

    completed

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use DB;
+use App\Models\User;
+use App\Models\Product;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->delete();
+        $data = [
+            [
+                'title' => 'Test Product 1',
+                'price' => 100,
+                'description' => 'Test Product Description 1',
+                'image' => null
+            ],
+            [
+                'title' => 'Test Product 2',
+                'price' => 200,
+                'description' => 'Test Product Description 2',
+                'image' => null
+            ]
+        ];
+        Product::insert($data);
+
+        // crete dummpy products
+        Product::factory(30)->create();
+
+
+        # create user
+        DB::table('users')->delete();
+        $data = [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('123456')
+        ];
+        User::create($data);
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Auth\AuthController;
+use App\Http\Controllers\Api\V1\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(['prefix' => 'v1' ], function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    # All Products
+    Route::get('products/all', [ProductController::class, 'all']);
+    Route::get('product/search', [ProductController::class, 'search']);
+
+    Route::group(['middleware' => 'auth:api'], function() {
+    	# Auth Routes
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('profile', [AuthController::class, 'profile']);
+        # Product Routes
+	    Route::resource('products', ProductController::class);
+        Route::get('products/{id}', [ProductController::class, 'show']);
+        Route::get('product/search-my-store', [ProductController::class, 'searchMyStore']);
+	});
+
+});
+
